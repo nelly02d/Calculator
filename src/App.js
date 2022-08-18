759455f62f7abc6726e9b537796b3728d6445451
Arnel <nelly02d@gmail.com>
2022-08-18 15:56:41 +0100

    Add HTML to DOM

--- src/App.js
+++ src/App.js
@@ -9,8 +9,39 @@ class App extends React.Component {
     }
 
   };
-
-  
+  render() {
+    return(
+      <div className='app'>
+        <div className='calculator'>
+          <div className='output'>
+            <div className='previous-operand'>1234*</div>
+            <div className='current-operand'>1234</div>
+          </div>
+          <div className='operator'>
+            <button id='clear'>DEL</button>
+            <button id='divide'>÷</button>
+            <button id='multiply'>x</button>
+            <button id='add'>+</button>
+            <button>-</button>
+          </div>
+          <div className='digits'>
+            <button id='one'>1</button>
+            <button id='two'>2</button>
+            <button id='three'>3</button>
+            <button id='four'>4</button>
+            <button id='five'>5</button>
+            <button id='six'>6</button>
+            <button id='seven'>7</button>
+            <button id='eight'>8</button>
+            <button id='nine'>9</button>
+            <button id='zero'>0</button>
+            <button id='decimals'>.</button>
+            <button id='equals'>=</button>
+          </div>
+        </div>
+    </div> 
+    )
+  }
 };
 
 
