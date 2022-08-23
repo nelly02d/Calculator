1ad55e1f1f72e798569300c3260467dc0d3b4638
Arnel <nelly02d@gmail.com>
2022-08-23 12:08:02 +0100

    add display function to preview number input on display menu

--- src/App.js
+++ src/App.js
@@ -7,42 +7,65 @@ class App extends React.Component {
     this.state = {
       currentInput: '',
       result: ''
+    };
+
+    this.handleDisplay = this.handleDisplay.bind(this);
+  };
+
+  handleDisplay = (e) => {
+    const value = e.target.textContent;
+
+    if (value === 'C') {
+      this.setState({
+        currentInput: ''
+      })
+    } else {
+      this.setState(state => ({
+        currentInput: this.state.currentInput + value
+      }));
     }
 
-  };
+    
+   
+//   handleCalculation = () => {
+    
+// ;  };
+
+  }
+
   render() {
     return(
       <div className='app'>
         <div className='calculator'>
-          <div className='output'>
-            <div className='previous-operand'>1234*</div>
-            <div className='current-operand'>1234</div>
+          <div id='display'>
+            <div id='previous-operand'></div>
+            <div id='current-input'>{ this.state.currentInput || 0 }</div>
           </div>
           <div className='operator'>
-            <button id='clear'>DEL</button>
-            <button id='divide'>÷</button>
-            <button id='multiply'>x</button>
-            <button id='add'>+</button>
-            <button>-</button>
+            <button id='clear' onClick={ this.handleDisplay }>C</button>
+            <button id='divide' onClick={ this.handleDisplay }>÷</button>
+            <button id='multiply' onClick={ this.handleDisplay }>x</button>
+            <button id='add' onClick={ this.handleDisplay }>+</button>
+            <button id='subtract' onClick={ this.handleDisplay }>-</button>
           </div>
           <div className='digits'>
-            <button id='one'>1</button>
-            <button id='two'>2</button>
-            <button id='three'>3</button>
-            <button id='four'>4</button>
-            <button id='five'>5</button>
-            <button id='six'>6</button>
-            <button id='seven'>7</button>
-            <button id='eight'>8</button>
-            <button id='nine'>9</button>
-            <button id='zero'>0</button>
-            <button id='decimals'>.</button>
+            <button id='one' onClick={ this.handleDisplay }>1</button>
+            <button id='two' onClick={ this.handleDisplay }>2</button>
+            <button id='three' onClick={ this.handleDisplay }>3</button>
+            <button id='four' onClick={ this.handleDisplay }>4</button>
+            <button id='five' onClick={ this.handleDisplay }>5</button>
+            <button id='six' onClick={ this.handleDisplay }>6</button>
+            <button id='seven' onClick={ this.handleDisplay }>7</button>
+            <button id='eight' onClick={ this.handleDisplay }>8</button>
+            <button id='nine' onClick={ this.handleDisplay }>9</button>
+            <button id='zero' onClick={ this.handleDisplay }>0</button>
+            <button id='decimals' onClick={ this.handleDisplay }>.</button>
             <button id='equals'>=</button>
           </div>
         </div>
     </div> 
-    )
-  }
+    );
+  };
 };
 
 
