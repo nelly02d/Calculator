5d06fd4bead6ad9819949884dbaadfab311ad695
Arnel <nelly02d@gmail.com>
2023-02-03 01:36:40 +0000

    add border style on button

--- src/App.js
+++ src/App.js
@@ -7,94 +7,85 @@ class App extends React.Component {
     this.state = {
       currInput: '',
       prevInput: '',
-      operand: ['+', '-', '/', 'x']
+      operation: undefined
     };
 
     this.handeNumbers = this.handeNumbers.bind(this);
     this.handleOperators = this.handleOperators.bind(this);
     this.calculate = this.calculate.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   };
 
-  
-  handeNumbers = (e) => {
-    const value = e.target.textContent;
-
-    if(value === '.' && this.state.currInput.includes('.')) return;
-    if(value === '.' && this.state.currInput === '') return;
-
-    if (value === 'C') {
-      this.setState({
-        currInput: '',
-        prevInput: ''
-      })
-    } else {
-      this.setState(state => ({
-        currInput: this.state.currInput.toString() + value.toString()
-      }));
-    }
+  //handle to reset state to null
+  handleReset = () => {
+    this.setState({
+      currInput: '',
+      prevInput: '',
+    })
   }
 
-  handleOperators = (e) => {
-    const value = e.target.textContent;
-    if(this.state.operand.includes(value) && this.state.currInput === '') return;
+  //handle when number is pressed
+  handeNumbers = (event) => {
+    if(event.currentTarget.value === '.' && this.state.currInput.includes('.')) return;
+    if(event.currentTarget.value === '.' && this.state.currInput === '') return;
 
+    let stringInput = this.state.currInput.toString() + event.currentTarget.value.toString();
+    this.setState({
+      currInput: stringInput
+    })
+  }
+
+  handleOperators = (event) => {
+    if(this.state.currInput === '') return
     if(this.state.prevInput !== '') {
       this.calculate();
     };
     
-    this.setState(state => ({
-      prevInput: `${this.state.currInput} ${value}`,
-      currInput:''
-    }));
+    this.setState({
+      currInput: '',
+      prevInput: `${this.state.currInput} ${event.currentTarget.value}`,
+      operation: event.currentTarget.value
+    })
   }
 
 
   calculate = () => {
     let calculation;
+    let operator = this.state.operation
 
     const prev = parseFloat(this.state.prevInput);
     const curr = parseFloat(this.state.currInput);
     if(isNaN(prev) || isNaN(curr)) return;
 
-    // 
-
-    // for(let i = 0; i < this.state.operand.length; i++) {
-    //   switch(this.state.operand[i]) {
-    //     case '+': 
-    //       calculation = prev - curr
-    //       break;
-    //     case '-': 
-    //       calculation = prev - curr
-    //       break;
-    //     case '*': 
-    //       calculation = prev - curr
-    //       break;
-    //     case '/': 
-    //       calculation = prev - curr
-    //       break;
-    //     default:
-    //       return;
-    //   }
+    // if(operator === '+') {
+    //   calculation = prev + curr;
+    // } else if(operator === '-') {
+    //   calculation = prev - curr;
+    // } else if(operator === 'x') {
+    //   calculation = prev * curr;
+    // } else if(operator === '/') {
+    //   calculation = prev / curr;
     // }
 
-    
-    this.state.operand.forEach(operator => {
-      if(operator === '+') {
-        calculation = prev + curr
-      } else if(operator === '-') {
+    switch(operator) {
+      case '+':
+        calculation = prev + curr;
+      break;
+      case '-':
         calculation = prev - curr;
-      } else {
-        return
-      }
+      break;
+      case 'x':
+        calculation = prev * curr;
+      break;
+      case '/':
+        calculation = prev / curr;
+      break;
+    }
 
-      console.log(operator)
-    });
-
-
-    this.setState(state => ({
-      prevInput: calculation ,
-      currInput: ''
-    }));
+    this.setState({
+      currInput: calculation,
+      prevInput: ''
+    })
   }
 
   render() {
@@ -106,25 +97,25 @@ class App extends React.Component {
             <div id='current-input'>{ this.state.currInput }</div>
           </div>
           <div className='operator'>
-            <button id='clear' onClick={ this.handeNumbers }>C</button>
-            <button id='divide' onClick={ this.handleOperators }>/</button>
-            <button id='multiply' onClick={ this.handleOperators }>x</button>
-            <button id='add' onClick={ this.handleOperators }>+</button>
-            <button id='subtract' onClick={ this.handleOperators }>-</button>
+            <button value='c' id='clear' onClick={ this.handleReset }>C</button>
+            <button value='/' id='divide' onClick={ this.handleOperators }>/</button>
+            <button value='x' id='multiply' onClick={ this.handleOperators }>x</button>
+            <button value='+' id='add' onClick={ this.handleOperators }>+</button>
+            <button value='-' id='subtract' onClick={ this.handleOperators }>-</button>
           </div>
           <div className='digits'>
-            <button id='one' onClick={ this.handeNumbers }>1</button>
-            <button id='two' onClick={ this.handeNumbers }>2</button>
-            <button id='three' onClick={ this.handeNumbers }>3</button>
-            <button id='four' onClick={ this.handeNumbers }>4</button>
-            <button id='five' onClick={ this.handeNumbers }>5</button>
-            <button id='six' onClick={ this.handeNumbers }>6</button>
-            <button id='seven' onClick={ this.handeNumbers }>7</button>
-            <button id='eight' onClick={ this.handeNumbers }>8</button>
-            <button id='nine' onClick={ this.handeNumbers }>9</button>
-            <button id='zero' onClick={ this.handeNumbers }>0</button>
-            <button id='decimals' onClick={ this.handeNumbers }>.</button>
-            <button id='equals' onClick={ this.calculate }>=</button>
+            <button value='1' id='one' onClick={ this.handeNumbers }>1</button>
+            <button value='2' id='two' onClick={ this.handeNumbers }>2</button>
+            <button value='3' id='three' onClick={ this.handeNumbers }>3</button>
+            <button value='4' id='four' onClick={ this.handeNumbers }>4</button>
+            <button value='5' id='five' onClick={ this.handeNumbers }>5</button>
+            <button value='6' id='six' onClick={ this.handeNumbers }>6</button>
+            <button value='7' id='seven' onClick={ this.handeNumbers }>7</button>
+            <button value='8' id='eight' onClick={ this.handeNumbers }>8</button>
+            <button value='9' id='nine' onClick={ this.handeNumbers }>9</button>
+            <button value='0' id='zero' onClick={ this.handeNumbers }>0</button>
+            <button value='.' id='decimals' onClick={ this.handeNumbers }>.</button>
+            <button value='=' id='equals' onClick={ this.calculate }>=</button>
           </div>
         </div>
     </div> 
